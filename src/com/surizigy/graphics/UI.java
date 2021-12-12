97a2b9470e609f597a597fb09f0cedfce05f93b9
Facundo <facundo.labarque@gmail.com>
2021-12-12 10:10:31 -0300

    c14 100 3-test

--- src/com/surizigy/graphics/UI.java
+++ src/com/surizigy/graphics/UI.java
@@ -35,10 +35,10 @@ public class UI {
 		if(Game.gameState == "PLAYING") {				
 			String digits = ""+(int)Game.score;			
 			for (int i = 0; i < digits.length(); i++) {				
-				/*if(i != (digits.length() - 1)*10) {				
-					g.drawImage(BIG_NUMBER[digits.charAt(i) - '0'], Game.WIDTH*Game.SCALE/2 - 18*Game.SCALE - 37, Game.HEIGHT*Game.SCALE/4, 18*2, 25*2, null);
-				}
-				else*/ if(i != digits.length() - 1) {				
+				//if(i != (digits.length() - 1)*100) {				
+					//g.drawImage(BIG_NUMBER[digits.charAt(i) - '0'], Game.WIDTH*Game.SCALE/2 - 18*Game.SCALE - 37, Game.HEIGHT*Game.SCALE/4, 18*2, 25*2, null);
+				//}
+				if(i != digits.length() - 1) {				
 					g.drawImage(BIG_NUMBER[digits.charAt(i) - '0'], Game.WIDTH*Game.SCALE/2 - 18*Game.SCALE - 1, Game.HEIGHT*Game.SCALE/4, 18*2, 25*2, null);
 				}else {
 					g.drawImage(BIG_NUMBER[digits.charAt(i) - '0'], Game.WIDTH*Game.SCALE/2 - 18, Game.HEIGHT*Game.SCALE/4, 18*2, 25*2, null);
@@ -47,6 +47,29 @@ public class UI {
 			//g.drawString(""+(int)Game.score, Game.WIDTH*Game.SCALE/2 - 15, Game.HEIGHT*Game.SCALE/4);		
 				}				
 			}
+			
+			/*char[] digits = (""+(int)Game.score).toCharArray();
+			int digitsCount = digits.length;
+			for (int i = 0; i < digitsCount; i++) {
+				g.drawImage(BIG_NUMBER[digits[i]], Game.WIDTH*Game.SCALE/2 - 18, Game.HEIGHT*Game.SCALE/4, 18*2, 25*2, null);
+			}*/
+			
+			/*int widthOfImgs = 18*2;
+			int countMod = 0, temp = (int)Game.score;//assuming its an integer score
+			while (temp > 0){//given the score is always positive
+			    switch(temp % 10){
+			        case 0:
+			                //draw 1
+			                g.drawImage(BIG_NUMBER[0], (Game.WIDTH*Game.SCALE/2 - 18) - widthOfImgs * countMod, Game.HEIGHT*Game.SCALE/4, 18*2, 25*2, null);
+			                //curOffset += widthOfImage1;
+			                //dont recall the exact syntax daImage.width?
+			        case 2:
+			        //blah blah
+			    }
+			    ++countMod;// if the images are of constant width
+			    temp /= 10;
+			}*/
+			
 		}		
 	}
 	
