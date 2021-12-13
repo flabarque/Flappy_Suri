229a95262cb71ea06a5bd6a393dd9a6d7e7a7a2a
Facundo <facundo.labarque@gmail.com>
2021-12-13 14:35:30 -0300

    c16 TubeGenerator sprite height - 128

--- src/com/surizigy/world/TubeGenerator.java
+++ src/com/surizigy/world/TubeGenerator.java
@@ -15,11 +15,11 @@ public class TubeGenerator {
 		time++;
 		if(time == 105) {				
 			int heightup = Entity.rand.nextInt(90 - 5) + 5;
-			Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 96, 0, Entity.ZIGGY_96);
+			Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 128, 0, Entity.ZIGGY_96);
 			Game.entities.add(tubeup);
 			
 			int heightdown = heightup + Entity.rand.nextInt(10 - 5) + 5;
-			Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 96, 0, Entity.KIKI_96);
+			Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 128, 0, Entity.KIKI_96);
 			Game.entities.add(tubedown);
 					
 			time = 0;
@@ -27,11 +27,11 @@ public class TubeGenerator {
 		}else if (Game.score > 75) {
 			if(time == 35) {				
 				int heightup = Entity.rand.nextInt(65 - 5) + 5;
-				Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 96, 0, Entity.ZIGGY_96);
+				Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 128, 0, Entity.ZIGGY_96);
 				Game.entities.add(tubeup);
 				
 				int heightdown = heightup + Entity.rand.nextInt(35 - 5) + 5;
-				Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 96, 0, Entity.KIKI_96);
+				Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 128, 0, Entity.KIKI_96);
 				Game.entities.add(tubedown);
 						
 				time = 0;
@@ -39,11 +39,11 @@ public class TubeGenerator {
 		}else if (Game.score > 49) {
 			if(time == 35) {				
 				int heightup = Entity.rand.nextInt(65 - 5) + 5;
-				Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 96, 0, Entity.ZIGGY_96);
+				Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 128, 0, Entity.ZIGGY_96);
 				Game.entities.add(tubeup);
 				
 				int heightdown = heightup + Entity.rand.nextInt(35 - 5) + 5;
-				Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 96, 0, Entity.KIKI_96);
+				Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 128, 0, Entity.KIKI_96);
 				Game.entities.add(tubedown);
 						
 				time = 0;
@@ -51,11 +51,11 @@ public class TubeGenerator {
 		}else if (Game.score > 25) {
 			if(time == 70) {				
 				int heightup = Entity.rand.nextInt(90 - 5) + 5;
-				Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 96, 0, Entity.ZIGGY_96);
+				Tube tubeup = new Tube(Game.WIDTH, -heightup, 32, 128, 0, Entity.ZIGGY_96);
 				Game.entities.add(tubeup);
 				
 				int heightdown = heightup + Entity.rand.nextInt(10 - 5) + 5;
-				Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 96, 0, Entity.KIKI_96);
+				Tube tubedown = new Tube(Game.WIDTH, Game.HEIGHT - heightdown - 24, 32, 128, 0, Entity.KIKI_96);
 				Game.entities.add(tubedown);
 						
 				time = 0;
