943b8044cf436fc8fa595c1524977b8095bad6d8
Facundo <facundo.labarque@gmail.com>
2021-12-30 10:05:18 -0300

    c19 GameOver.java clean

--- src/com/surizigy/main/GameOver.java
+++ src/com/surizigy/main/GameOver.java
@@ -9,22 +9,21 @@ import com.surizigy.graphics.UI;
 
 public class GameOver extends Menu {
 
+	//initializing Variables
+	//images
 	public static BufferedImage GAME_OVER = Game.spritesheet.getSprite(128, 192, 66, 25);
 	public static BufferedImage BOARD = Game.spritesheet.getSprite(2, 267, 114, 82);
 	public static BufferedImage[] MEDALS = {Game.spritesheet.getSprite(182, 288, 18, 18), Game.spritesheet.getSprite(164, 288, 18, 18), Game.spritesheet.getSprite(146, 288, 18, 18), Game.spritesheet.getSprite(128, 288, 18, 18)};
 		
-	private boolean showMessage = true;
-	//private int framesGameOver = 0;	
-	
-	private int finalScore;
-	//private int bestScore;
+	private boolean showMessage = true;	
+	private int finalScore;	
 	private boolean getMedal = true;
 	
-	//private boolean scoreGreater = false;
-	
+	//initializing Loop
 	public void tick() {		
 		Sounds.musicBackground.stop(0);
 		
+		//showing Message
 		frames++;
 		if(frames == 30) {
 			frames = 0;
@@ -32,29 +31,23 @@ public class GameOver extends Menu {
 				showMessage = false;
 			else
 				showMessage = true;
-		}
+		}							
 		
-		//finalScore++;
-		/*if((int)Game.score == 0) {
-			finalScore = 0;			
-		}else {*/					
-		
-		if((int)Game.score > HighScore.bestScore()) {
-			//scoreGreater = true;
+		//adding new High Score
+		if((int)Game.score > HighScore.bestScore()) {			
 			HighScore.setNewBest((int)Game.score);
 		}
 					
-		//if((int)Game.score > 0) {
+		//adding Final Score
 		finalScore++;
 		Sounds.points.play(0.5);
 		
 		if(finalScore > (int)Game.score) {
-			finalScore = (int)Game.score;
-			//finalScore--;				
+			finalScore = (int)Game.score;							
 			Sounds.points.stop(0);			
-		}
-		//}			
-				
+		}					
+		
+		//showing Medals
 		if(getMedal) {				
 			if(finalScore == 25) {
 				getMedal = false;
@@ -73,58 +66,17 @@ public class GameOver extends Menu {
 				Sounds.medal.play(0.5);
 			}
 		}
-		//salvar novo highscore
-		/*if((int)Game.score > bestScore) {			
-			bestScore = (int)Game.score;
-			//scoreGreater = true;		
-			String[] opt1 = {"bestScore"}; //salvando o nivel, mas podemos colocar outros parametros, como vida, posição dos inimigos, etc.
-			int[] opt2 = {(int)Game.score};
-			HighScore.saveGame(opt1, opt2, 10); //neste caso o encode é igual a 10
-			System.out.println("New Best Score!");			
-		}else {
-			//scoreGreater = false;
-			File file = new File("save.txt");
-			if(file.exists()) {			
-				String saver = HighScore.loadGame(10); //10 é o encode que usamos
-				HighScore.applySave(saver);				
-			}
-		}*/
 		
-		
-		//ver highscore 
-		/*if(scoreGreater == false) {
-			String saver = HighScore.loadGame(10); //10 é o encode que usamos
-			HighScore.applySave(saver);
-		}*/
-		
-		
-		/*for (int i = 0; i < (int)Game.score; i++) {	
-			if (i < (int)Game.score) {
-				Sounds.points.play(0.5);
-			}else {
-				i = 0;
-			}		
-		}*/
-		/*if (finalScore < (int)Game.score) {
-			Sounds.points.play(0.5);
-			finalScore = (int)Game.score;
-		}else if (finalScore == (int)Game.score) {
-			finalScore = 0;
-			}*/
-					
 	}
 	
-	public void render(Graphics g) {
-		g.setColor(new Color(0, 0, 0, 100)); //opacidade
-		g.fillRect(0, 0, Game.WIDTH*Game.SCALE, Game.HEIGHT*Game.SCALE);
-		//g2.setFont(new Font("calibri", Font.BOLD, 50));
-		//g2.setColor(Color.white);
-		//g2.drawString("GAME OVER", ((WIDTH*SCALE)/2) - 115, ((HEIGHT*SCALE)/2) - 30);
-		g.drawImage(GAME_OVER, ((Game.WIDTH*Game.SCALE) / 2) - 33*Game.SCALE, ((Game.HEIGHT*Game.SCALE) / 4) - 90, 66*Game.SCALE, 25*Game.SCALE, null);
-		//g2.setFont(new Font("calibri", Font.BOLD, 30));
-		//g2.setColor(Color.white);
+	//rendering Game Over
+	public void render(Graphics g) {		
+		g.setColor(new Color(0, 0, 0, 100)); //darkness
+		g.fillRect(0, 0, Game.WIDTH*Game.SCALE, Game.HEIGHT*Game.SCALE);		
+		g.drawImage(GAME_OVER, ((Game.WIDTH*Game.SCALE) / 2) - 33*Game.SCALE, ((Game.HEIGHT*Game.SCALE) / 4) - 90, 66*Game.SCALE, 25*Game.SCALE, null);		
 		g.drawImage(BOARD, ((Game.WIDTH*Game.SCALE) / 2) - 57*Game.SCALE, ((Game.HEIGHT*Game.SCALE) / 2) - 41*Game.SCALE, 114*Game.SCALE, 82*Game.SCALE, null);
 		
+		//rendering Message
 		if(showMessage) {
 			g.setFont(new Font("calibri", Font.BOLD, 30));
 			g.setColor(Color.black);
@@ -134,6 +86,7 @@ public class GameOver extends Menu {
 			g.drawString(">Press SPACE to continue<", ((Game.WIDTH*Game.SCALE)/2) - 166, Game.HEIGHT*Game.SCALE - 121);
 		}
 		
+		//rendering Medals
 		if(finalScore >= 25) {
 			g.drawImage(MEDALS[0], ((Game.WIDTH*Game.SCALE) / 2) - 38*Game.SCALE, ((Game.HEIGHT*Game.SCALE) / 2) - 2*Game.SCALE, 18*Game.SCALE, 18*Game.SCALE, null);
 		}
@@ -147,6 +100,7 @@ public class GameOver extends Menu {
 			g.drawImage(MEDALS[3], ((Game.WIDTH*Game.SCALE) / 2) - 38*Game.SCALE, ((Game.HEIGHT*Game.SCALE) / 2) - 2*Game.SCALE, 18*Game.SCALE, 18*Game.SCALE, null);
 		}
 		
+		//rendering Final Score
 		String digits = ""+finalScore;			
 		for (int i = 0; i < digits.length(); i++) {					
 			if(i != digits.length() - 1) {				
@@ -155,7 +109,8 @@ public class GameOver extends Menu {
 				g.drawImage(UI.SMALL_NUMBER[digits.charAt(i) - '0'], Game.WIDTH*Game.SCALE - 153, Game.HEIGHT*Game.SCALE/2 - 45, 11*Game.SCALE, 14*Game.SCALE, null);
 			}				
 		}
-			
+		
+		//rendering High Score
 		String digits_1 = ""+HighScore.bestScore;			
 			for (int i = 0; i < digits_1.length(); i++) {				
 				if(i != digits_1.length() - 1) {				
